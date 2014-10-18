c589cb8f08a28944fa19bd6c848c64383714e3eb
Brian Picciano <mediocregopher@gmail.com>
2014-10-18 17:09:30 -0400

    changed my mind about types

--- types/types.go
+++ types/types.go
@@ -9,13 +9,3 @@ type Elem interface {
 // Number can either be either an Int or a Float
 type Number interface {
 }
-
-type Str string
-
-type Int int
-
-type Float float32
-
-type Char rune
-
-type Err error
